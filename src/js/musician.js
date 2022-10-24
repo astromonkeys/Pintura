cd38735ef28bd7ebce57136421f477d5bc7f5545
astrmonkeys <noah.zurn@gmail.com>
2022-10-24 16:16:09 -0500

    added playback progress,shuffle and repeat buttons

--- src/js/musician.js
+++ src/js/musician.js
@@ -13,9 +13,29 @@ var pinturaDevice;
 
 function configurePlayer() {
     // Ready
+
+    // set up web api wrapper
+    spotify = new SpotifyWebApi();
+    spotify.setAccessToken(localStorage.getItem("access_token"));
+
+    // TODO allow searching, selecting, and playing of user's playlists - the only music functionality I'm going to make for now
+    // - make play/pause, rewind and skip buttons do their jobs
+    // - update album artwork and song/artist names on song change
+    // - add song progress bar -> add scrubbing later
+
+    // retrieve current playlist/song/artist name on startup
+    setPlaybackState();
+    // gather playlists on startup
+    getMyPlaylists();
+    // sets up ui
+    makeButtonsWork();
+
     musician.addListener('ready', ({ device_id }) => {
         console.debug('Spotify player ready with Device ID', device_id);
         pinturaDevice = device_id;
+        // probably a better way to do this, but no listener exists afaik
+        setInterval(checkSongChange, 750);
+        updatePlaybackProgress();
     });
 
     // Not Ready
@@ -37,33 +57,7 @@ function configurePlayer() {
 
     musician.connect();
 
-    // set up web api wrapper
-    spotify = new SpotifyWebApi();
-    spotify.setAccessToken(localStorage.getItem("access_token"));
 
-    // add placeholder album artwork using the api
-
-    // TODO retrieve current playlist/song/artist name on startup
-
-    spotify.searchAlbums("dark side of the moon", {
-        type: "album",
-        limit: 2
-    }, function (errorObject, data) {
-        let art = data.albums.items[0].images[1].url;
-        // TODO in reality, resize image to card size
-        document.getElementById("album_art").src = art;
-    });
-
-    // TODO allow searching, selecting, and playing of user's playlists - the only music functionality I'm going to make for now
-    //  - make play/pause, rewind and skip buttons do their jobs
-    // - update album artwork
-    // - update song/artist names
-    // - add song progress bar -> add scrubbing later
-
-    // gather playlists on startup
-    getMyPlaylists();
-    // sets up ui
-    makeButtonsWork();
 }
 
 /* don't need to do anything with the data, just for debugging purposes */
@@ -73,14 +67,10 @@ function logApiResponse(errorObject, data) {
 }
 
 function playPlaylist(uri) {
-    /* leave shuffle on for now, TODO fix and add this feature later */
-    //spotify.setShuffle(true, { device_id: pinturaDevice }, function (errorObject, data) {
-    //    logApiResponse(errorObject, data);
-    //});
-
     // choooses the active device by default
-    spotify.play({ device_id: pinturaDevice, context_uri: uri }, function (errorObject, data) {
+    spotify.play({ device_id: pinturaDevice, context_uri: uri }, (errorObject, data) => {
         logApiResponse(errorObject, data);
+        setPlaybackState();
     });
 }
 
@@ -97,7 +87,7 @@ function selectPlaylist(event) {
 function getMyPlaylists() {
     let dropdown = document.getElementById("playlistSelect");
 
-    spotify.getUserPlaylists(secrets.PROFILE, { limit: 10 }, function (errorObject, data) {
+    spotify.getUserPlaylists(secrets.PROFILE, { limit: 10 }, (errorObject, data) => {
 
         for (playlist of data.items) {
             userPlaylists.push([playlist.name, playlist.uri, playlist.images[0].url]);
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,11 @@
 /* some ui stuff */
 
-var playing = false;
+const MS_SKIP_SEEK = 5000; // if you're more than this amount of time through the song, the rewind button will function as a seek to beginning instead of skip to prev song
+
+var playing;
+var currentTrack;
+var shuffleState = 0; // 0 = off/white image, 1 = on/dark image
+var repeatState = 0; // 0 = off/white image, 1 = context mode, 2 = track mode
 
 img_paths = {
     RW: "/icons/rw.svg",
@@ -10,18 +15,115 @@ img_paths = {
     PLAY: "/icons/play.svg",
     PLAY_FILL: "/icons/play-fill.svg",
     PAUSE: "/icons/pause.svg",
-    PAUSE_FILL: "/icons/pause-fill.svg"
+    PAUSE_FILL: "/icons/pause-fill.svg",
+    SHUFFLE: "/icons/shuffle.svg",
+    REPEAT: "/icons/repeat.svg",
+    REPEAT_TRACK: "/icons/repeat-1.svg",
+}
+
+function checkSongChange() {
+    //console.debug("Checking for song change");
+    spotify.getMyCurrentPlayingTrack({}, (errorObject, data) => {
+        if (currentTrack.uri != data.item.uri) {
+            setPlaybackState();
+        }
+    });
+
+    updatePlaybackProgress();
+}
+
+function updatePlaybackProgress() {
+    spotify.getMyCurrentPlayingTrack({}, (errorObject, data) => {
+        let progressPct = data.progress_ms / data.item.duration_ms * 100;
+        document.getElementById("playback-progress").style.width = progressPct + "%";
+    });
+}
+
+function setPlaybackState() {
+    spotify.getMyCurrentPlaybackState({}, (errorObject, data) => {
+        // logApiResponse(errorObject, data);
+
+        currentTrack = data.item;
+        document.getElementById("songTitle").innerHTML = currentTrack.name;
+
+        let artistStr = "";
+        for (let i = 0; i < data.item.artists.length; i++) {
+            artistStr = artistStr.concat(data.item.artists[i].name, ", ");
+        }
+        artistStr = artistStr.substring(0, artistStr.length - 2);
+        document.getElementById("artistName").innerHTML = artistStr;
+
+        document.getElementById("album_art").src = data.item.album.images[0].url;
+
+        // update the "now playing from" field
+        if (data.context != null) {
+            let nowPlaying = "";
+            let id = data.context.uri.split(":")[2];
+            switch (data.context.type) {
+                // in each case, get the name
+                case "playlist":
+                    spotify.getPlaylist(id, {}, function (errorObject, data) {
+                        document.getElementById("nowPlaying").innerHTML = data.name;
+                    });
+                    break;
+                case "artist":
+                    spotify.getArtist(id, {}, function (errorObject, data) {
+                        document.getElementById("nowPlaying").innerHTML = data.name;
+                    });
+                    break;
+                case "album":
+                    spotify.getAlbum(id, {}, function (errorObject, data) {
+                        document.getElementById("nowPlaying").innerHTML = data.name;
+                    });
+                    break;
+                default:
+                    console.debug("unrecognized media type");
+                    break;
+            }
+        }
+
+        /* update button icons based on play/paused, shuffling, etc. */
+        if (data.is_playing) {
+            document.getElementById("playpausebtn").childNodes[0].src = img_paths.PAUSE;
+            playing = true;
+        } else {
+            document.getElementById("playpausebtn").childNodes[0].src = img_paths.PLAY;
+            playing = false;
+        }
+
+        if (data.shuffle_state) shuffleState = 1;
+        else shuffleState = 0;
+
+        switch (data.repeat_state) {
+            case "off":
+                break;
+            case "context":
+                repeatState = 1;
+                break;
+            case "track":
+                repeatState = 2;
+                break;
+            default:
+                console.debug("Invalid repeat state");
+                break;
+        }
+
+        checkShuffle();
+        checkRepeat();
+    });
 }
 
 /* could not for the life of me find a more concise way to do this, since I want to handle a bunch of listeners */
 function makeButtonsWork() {
     let b = document.getElementById("playpausebtn");
-    b.addEventListener("click", function () {
+    b.addEventListener("click", () => {
         // toggle play, and change image to pause
 
         // maybe call another function to help with this if there's more to do
-        musician.togglePlay();
         playing = !playing;
+        if (!playing) {
+            spotify.pause({}, null);
+        } else { spotify.play({}, null) }
 
         let path = b.childNodes[0].src.split('5500')[1];
         if (playing) path = img_paths.PAUSE_FILL;
@@ -29,7 +131,7 @@ function makeButtonsWork() {
 
         b.childNodes[0].src = path;
     });
-    b.addEventListener("mouseenter", function () {
+    b.addEventListener("mouseenter", () => {
         // change image
         /* THIS WILL NEED TO BE CHANGED AFTER DEPLOYMENT */
         let path = b.childNodes[0].src.split('5500')[1];
@@ -39,7 +141,7 @@ function makeButtonsWork() {
 
         b.childNodes[0].src = path;
     });
-    b.addEventListener("mouseleave", function () {
+    b.addEventListener("mouseleave", () => {
         // change image
         /* THIS WILL NEED TO BE CHANGED AFTER DEPLOYMENT */
         let path = b.childNodes[0].src.split('5500')[1];
@@ -51,30 +153,101 @@ function makeButtonsWork() {
     });
 
     let r = document.getElementById("rewindbtn");
-    r.addEventListener("click", function () {
+    r.addEventListener("click", () => {
         // skip song backward
         console.debug("skipping song backward");
+        handleSeekSkip();
+        setPlaybackState();
     });
-    r.addEventListener("mouseenter", function () {
+    r.addEventListener("mouseenter", () => {
         // change image
         r.childNodes[0].src = img_paths.RW_FILL;
     });
-    r.addEventListener("mouseleave", function () {
+    r.addEventListener("mouseleave", () => {
         // change image
         r.childNodes[0].src = img_paths.RW;
     });
 
     let f = document.getElementById("forwardbtn");
-    f.addEventListener("click", function () {
+    f.addEventListener("click", () => {
         // skip song forward
         console.debug("skipping song foward");
+        spotify.skipToNext({}, (errorObject, data) => {
+            logApiResponse(errorObject, data);
+        });
+        setPlaybackState();
     });
-    f.addEventListener("mouseenter", function () {
+    f.addEventListener("mouseenter", () => {
         // change image
         f.childNodes[0].src = img_paths.FF_FILL;
     });
-    f.addEventListener("mouseleave", function () {
+    f.addEventListener("mouseleave", () => {
         // change image
         f.childNodes[0].src = img_paths.FF;
     });
+
+    // TODO make these icons(all icons, but these for starters) more sophisticated/colorful
+    let sh = document.getElementById("shufflebtn");
+    sh.addEventListener("click", checkShuffle);
+
+    let re = document.getElementById("repeatbtn");
+    re.addEventListener("click", checkRepeat);
+}
+
+function handleSeekSkip() {
+    // if song is within 5 seconds, skip backwards. else, seek to beginning
+    spotify.getMyCurrentPlayingTrack({}, (errorObject, data) => {
+        let position = data.progress_ms;
+        if (position <= MS_SKIP_SEEK) {
+            spotify.skipToPrevious({}, (errorObject, data) => {
+                logApiResponse(errorObject, data);
+            });
+        } else {
+            spotify.seek(0, {}, (errorObject, data) => {
+                logApiResponse(errorObject, data);
+            });
+        }
+    });
+}
+
+// TODO add api calls to these functions
+function checkShuffle() {
+    if (shuffleState == 0) {
+        // shuffle is now off
+        //spotify.setShuffle(true, { device_id: pinturaDevice }, function (errorObject, data) {
+        //    logApiResponse(errorObject, data);
+        //});
+        document.getElementById("shuffle_img").style.filter = "invert(100%)";
+        shuffleState = 1;
+    } else {
+        // shuffle is now on
+        document.getElementById("shuffle_img").style.filter = "invert(0%)";
+        shuffleState = 0;
+    }
+}
+
+
+function checkRepeat() {
+    switch (repeatState) {
+        case 0:
+            // repeat is now off
+            document.getElementById("repeat").src = img_paths.REPEAT;
+            document.getElementById("repeat").style.filter = "invert(100%)";
+            repeatState = 1;
+            break;
+        case 1:
+            // context repeat is now on
+            document.getElementById("repeat").style.filter = "invert(0%)";
+            repeatState = 2;
+            break;
+        case 2:
+            // track repeat is now on
+            document.getElementById("repeat").style.filter = "invert(0%)";
+            document.getElementById("repeat").src = img_paths.REPEAT_TRACK;
+            repeatState = 0;
+            break;
+        default:
+            console.debug("Invalid repeat state");
+            break;
+    }
 }
